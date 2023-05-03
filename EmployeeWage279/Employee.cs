fddfe8e5375696f4eb3c3bbf1e60f920412edaa6
patelaftab <10patelaftab@gmail.com>
2023-05-03 12:52:39 +0530

    Adding a Part Time Employee And Checking Daily Wage

--- EmployeeWage279/Employee.cs
+++ EmployeeWage279/Employee.cs
@@ -11,14 +11,20 @@ namespace EmployeeWage279
         public static void EmployeeInfo()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
             int empHrs = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present Full Time");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is Present Part Time");
             }
             else
             {
