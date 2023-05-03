aa00967e11534eb4bfa2216c573d56a12785eb42
patelaftab <10patelaftab@gmail.com>
2023-05-03 12:59:54 +0530

    Executing Using Switch Statment

--- EmployeeWage279/Employee.cs
+++ EmployeeWage279/Employee.cs
@@ -8,31 +8,32 @@ namespace EmployeeWage279
 {
     public class Employee
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HR = 20;
         public static void EmployeeInfo()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HR = 20;
+
             int empHrs = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is Present Full Time");
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is Present Part Time");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Present Full Time");
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Present Part Time");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             int empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Employe Daily Wage is: "+empWage);
+            Console.WriteLine("Employe Daily Wage is: " + empWage);
         }
     }
 }
