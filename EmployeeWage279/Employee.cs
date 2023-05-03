511f28e07e32e87a0d9c3789bea35efa4ad467ef
patelaftab <10patelaftab@gmail.com>
2023-05-03 12:46:37 +0530

    Calculating Employee Daily Wage

--- EmployeeWage279/Employee.cs
+++ EmployeeWage279/Employee.cs
@@ -11,13 +11,22 @@ namespace EmployeeWage279
         public static void EmployeeInfo()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is Present");
             }
-            else { Console.WriteLine("Employee is Absent"); }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
+            int empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employe Daily Wage is: "+empWage);
         }
     }
 }
